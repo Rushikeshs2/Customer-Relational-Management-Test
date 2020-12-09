f472cca5596249ac9ee00c766caa6efd10153494
Rushikesh Susar <64800815+Rushikeshs2@users.noreply.github.com>
2020-12-09 12:38:22 +0530

    Update settings.py

--- crash/settings.py
+++ crash/settings.py
@@ -80,13 +80,10 @@ WSGI_APPLICATION = 'crash.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'DEMO_TEST',
-        'USER': 'postgres',
-        'PASSWORD':'Rushi@2000',
-        'HOST':'localhost','*',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 }
 
 
